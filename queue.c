fec8e90be640f458be2f9b04fe1d4e8f78b2f40c
allenliao <liao4792856@gmail.com>
2024-03-18 22:25:38 +0800

    Modify q_insert_head() function Use macro strdup to simplify porcess of coping string.

--- queue.c
+++ queue.c
@@ -39,19 +39,16 @@ void q_free(struct list_head *l)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
-    if (!head) {
+    if (!head)
         return false;
-    }
     element_t *new = malloc(sizeof(element_t));
-    if (!new) {
+    if (!new)
         return false;
-    }
-    new->value = malloc(strlen(s) + 1);
+    new->value = strdup(s);
     if (!new->value) {
         free(new);
         return false;
     }
-    strncpy(new->value, s, strlen(s) + 1);
     list_add(&new->list, head);
     return true;
 }
